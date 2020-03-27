113d8f158741facf0187f4c90d243fdb1aa407ec
Xavier Vivancos García <x.vivancos91@hotmail.com>
2020-03-27 23:46:14 +0100

    third

--- R/bus.stations.R
+++ R/bus.stations.R
@@ -1,3 +1,5 @@
+
+
 bus.stations <- function(app_id, app_key, line){
 
   # If you don't specify the line, the function retrieves data from all bus stations
@@ -9,24 +11,35 @@ bus.stations <- function(app_id, app_key, line){
     url3 <- paste0(url1, app_id, url2, app_key)
 
     # Data set
-    dataset <- jsonlite::fromJSON(url3, flatten=TRUE)
-    dataset$features
+    dataset <- jsonlite::fromJSON(url3, flatten=TRUE)$features
 
   # The function retrieves data from bus stations corresponding to the specified line
-  } else{
+  }else{
 
     # URL containing the requested data set
     url1 <- "https://api.tmb.cat/v1/transit/linies/bus/"
-    url2 <- line
-    url3 <- "/parades?app_id="
-    url4 <- "&app_key="
-    url5 <- paste0(url1, url2, url3, app_id, url4, app_key)
+    url2 <- "/parades?app_id="
+    url3 <- "&app_key="
+    url4 <- paste0(url1, line, url2, app_id, url3, app_key)
 
     # Data set
-    dataset <- jsonlite::fromJSON(url5, flatten=TRUE)
-    dataset$features
+    dataset <- jsonlite::fromJSON(url4, flatten=TRUE)$features
 
   }
+
+  latitude <- vector()
+  longitude <- vector()
+
+  # Latitude and longitude in separate columns
+  for(i in 1:nrow(dataset)){
+
+    latitude[i] <- dataset$geometry.coordinates[[i]][1]
+    longitude[i] <- dataset$geometry.coordinates[[i]][2]
+
+  }
+
+  dataset <- cbind(dataset, latitude, longitude) %>%
+    dplyr::select()
 
 }
 
